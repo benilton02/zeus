6d06c1cb4529801c0da4a1af993f325a17c4f13d
benilton02 <bsa.ele@uea.edu.br>
2022-07-22 20:05:52 -0400

    chore: add env

--- core_app/views.py
+++ core_app/views.py
@@ -3,13 +3,12 @@ from rest_framework.decorators import api_view
 from core_app.models import Weather
 from core_app.weather import weather_async
 from asgiref.sync import sync_to_async
+from core_app.set_city_id import city_ids
+
 
 
 @api_view(['POST'])
 def save_weather(request, user_id):
-    # q = sync_to_async(get_report, thread_sensitive=True)
-    # print("\n\n***filter async",q, "***\n\n")  
-
     try:
         queryset = Weather.objects.filter(user_id=user_id)
     
@@ -24,10 +23,12 @@ def save_weather(request, user_id):
             'message': "user_id already exists",
         }
         return JsonResponse(data, status=400)
+    
     try: 
         weather_async(user_id)
         data = {"message":"collecting data"}
         return JsonResponse(data, status=200)
+    
     except Exception as e:
         print(e)
         return JsonResponse(dict(), status=200)
@@ -38,15 +39,15 @@ def report_weather(request, user_id):
     queryset = Weather.objects.filter(user_id=user_id)
 
     if queryset:
-        num  = float(queryset.all().count())
+        num  = queryset.all().count()
         
-        den = 167.0 #total de cidades
-        progress = num / den
+        den = len(city_ids)
+        progress = float(num / den)
         progress *= 100
 
         data = {
             "user_id": user_id,
-            "progress":"{:.2f}".format(progress)
+            "progress":"{:.2f}%".format(progress)
             }
 
         status = 200 
